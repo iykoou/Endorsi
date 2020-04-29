93180b5d089dcee92217dfddbc5c10479de067d6
Akito Mizukito <akito@evolution-x.org>
2020-04-29 19:19:34 +0900

    feds: Initial rework of federation
    Change-Id: I06c14f89005334f71a0322d9e4c169672dee42e0
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- haruka/modules/android.py
+++ haruka/modules/android.py
@@ -33,9 +33,6 @@ from requests import get
 # This module was inspired by Android Helper Bot by Vachounet.
 # None of the code is taken from the bot itself, to avoid confusion.
 
-LOGGER.info("android: Original Android Modules by @RealAkito on Telegram")
-
-
 @register(pattern=r"^/los(?: |$)(\S*)")
 async def los(event):
     if event.from_id == None:
